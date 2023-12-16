f39d510783bf14b89e6b0e19fe4c21c3c4887ae0
abdel124 <touazi.abdelhak@gmail.com>
2023-12-16 12:15:35 -0800

    fix s3 region

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -29,7 +29,7 @@ terraform {
   backend "s3" {
     bucket = "vprofileactions124"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-west-1"
   }
 
   required_version = "~> 1.6.3"
